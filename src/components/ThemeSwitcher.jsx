a9ec727b05a166f4d2d0e6d92da40f18c5ef25ff
Zairui Yang <zairuiy@upenn.edu>
2024-06-19 17:26:17 -0400

    complete ThemeSwitcher Styling

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -1,19 +1,16 @@
 import React, { useEffect, useState } from 'react';
 
 const ThemeSwitcher = () => {
-  const [theme, setTheme] = useState('light'); // Set default theme to 'light'
+  const [theme, setTheme] = useState('dark'); // Set default theme to 'dark'
 
   useEffect(() => {
     const themeLink = document.getElementById('theme-link');
-    const htmlElement = document.documentElement;
     if (theme === 'dark') {
       themeLink.href = '/darkTheme.css';
-      htmlElement.classList.remove('light-mode');
-      htmlElement.classList.add('dark-mode');
+      document.body.classList.remove('light-mode');
     } else {
       themeLink.href = '/lightTheme.css';
-      htmlElement.classList.remove('dark-mode');
-      htmlElement.classList.add('light-mode');
+      document.body.classList.add('light-mode');
     }
   }, [theme]);
 
@@ -22,7 +19,7 @@ const ThemeSwitcher = () => {
   };
 
   return (
-    <button onClick={toggleTheme}>
+    <button className="theme-switcher" onClick={toggleTheme}>
       Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
     </button>
   );

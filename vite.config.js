2786909f695f6105d6c69741e9d2a736e1c72251
Zairui Yang <zairuiy@upenn.edu>
2024-06-20 14:15:01 -0400

    Fix paths for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/zairuiy-personal-portfolio/',  // repo name
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  }
-});
+  base: '/zairuiy-personal-portfolio/' // repository name
+})

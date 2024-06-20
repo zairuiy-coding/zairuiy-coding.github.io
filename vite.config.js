05dd6f79d6aab14d90d7d879703f46d555104097
Zairui Yang <zairuiy@upenn.edu>
2024-06-20 16:31:43 -0400

    remove base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/zairuiy-personal-portfolio/' // repository name
+//   base: '/zairuiy-personal-portfolio/' // repository name
 })
